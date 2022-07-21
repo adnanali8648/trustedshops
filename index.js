630ae2edd3328c5ae7b326a8c5464c660932b810
adnanali8648 <adnan.ali8648@gmail.com>
2022-07-21 10:35:32 +0200

    added prompt-sync-history

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var prompt = require("prompt-sync")();
+var prompt = require("prompt-sync")({
+  history: require("prompt-sync-history")(), //open history file
+});
 
 const name = prompt("whats your name?");
 
 console.log(`Good Morning ${name}`);
+prompt.history.save();
